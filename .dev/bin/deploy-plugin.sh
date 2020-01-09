1c372e629e13b9545aa5b790a4743879b3c1e420
Evan Herman <evan.m.herman@gmail.com>
2020-01-09 18:24:01 -0500

    Test

--- .dev/bin/deploy-plugin.sh
+++ .dev/bin/deploy-plugin.sh
@@ -25,4 +25,4 @@
 CHANGELOG=$(sed -n -e '/== Changelog ==/,$p' $HOME/project/readme.txt | tail -n +3)
 
 # Deploy a Coblocks Github release
-ghr -t ${GH_ACCESS_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -b "${CHANGELOG}" -delete ${CIRCLE_TAG} /tmp/artifacts
+ghr -t ${GH_ACCESS_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -b "${CHANGELOG}" ${CIRCLE_TAG} /tmp/artifacts
